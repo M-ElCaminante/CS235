6e44f97ca770069ac75e609eeeb31246667f2d02
M-ElCaminante <70738162+M-ElCaminante@users.noreply.github.com>
2021-01-13 18:54:02 -0700

    Add files via upload

---
+++ Lab4/lab4/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include "Pathfinder.h"
+
+int main(int argc, char *argv[]) {
+
+	Pathfinder* pathptr = NULL;//the Pathfinder
+	// Test 2D array
+	string filename = argv[1];
+    ifstream in(argv[1]);
+	pathptr = new Pathfinder();
+	pathptr->importMaze(filename);
+	cout << pathptr->toString();
+	pathptr->solveMaze(filename);
+	exit(0);
+	// End of Test 2D array
+}
